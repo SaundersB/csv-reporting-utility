8372e7e1f43ba8f46946e1630f9075d454a30911
bsaunders <0pointy1>
2019-11-21 13:33:05 -0800

    Adding a simple CSVReportingUtility to create a CSV file from a data array.

---
+++ CSVReportUtility.php
@@ -0,0 +1,50 @@
+<?php
+
+class CSVReportUtility
+{
+
+    /**
+     * @param $path
+     * @param  $rows
+     * @param  $filename
+     * @return string|null
+     */
+    public function writeDataToCSV($path, $rows, $filename)
+    {
+        if (!isset($rows) || sizeof($rows) <= 0) {
+            return null;
+        }
+        try {
+            if (!is_dir($path)) {
+                mkdir($path, 0755, true);
+            }
+            $firstElement = $rows[0];
+            if (!is_array($firstElement)) {
+                $firstElement = $firstElement->toArray();
+            }
+            $fullFileName = $path . $filename;
+            $file = fopen($fullFileName, 'w');
+            fputcsv($file, array_keys($firstElement));
+            foreach ($rows as $row) {
+                if (!is_array($row)) {
+                    fputcsv($file, $row->toArray());
+                } else {
+                    fputcsv($file, $row);
+                }
+            }
+            fclose($file);
+            return $fullFileName;
+        } catch (\Exception $exception) {
+            return null;
+        }
+    }
+
+    public function deleteCSVReport($path){
+        try {
+            unlink($path);
+        } catch (Exception $exception){
+            return null;
+        }
+    }
+
+}
